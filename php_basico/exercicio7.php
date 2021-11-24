4405f0cf0e45ef41208e4b10ef15ebc56551113c
Lucas <lubapisilva@gmail.com>
2021-11-24 01:37:06 -0300

    media ponderada

---
+++ php_basico/exercicio7.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Zé ferreira";
+
+$nota1 = 7.2;
+$nota2 = 3.1;
+$nota3 = 5;
+
+$media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 5)) / (2 + 3 + 5);
+
+echo "O aluno: $nome esta com media: $media";
