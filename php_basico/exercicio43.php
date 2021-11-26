ad91d80422189732decb1f9d67bed8ba19a0afb8
Lucas <lubapisilva@gmail.com>
2021-11-26 15:56:17 -0300

    pathinfo imagens

---
+++ php_basico/exercicio43.php
@@ -0,0 +1,14 @@
+<?php
+
+$img1 = pathinfo('imagens/panda.jpg');
+$img2 = pathinfo('imagens/cachorro');
+$img3 = pathinfo('imagens/download.jfif');
+$img4 = pathinfo('imagens/cat-528386_1280-1024x768');
+
+var_dump($img1);
+echo "<br><br>";
+var_dump($img2);
+echo "<br><br>";
+var_dump($img3);
+echo "<br><br>";
+var_dump($img4);
