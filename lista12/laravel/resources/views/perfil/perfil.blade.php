bc1c98139ac7711bfcae056b9cbdd939049fa9b2
Lucas <lubapisilva@gmail.com>
2021-12-07 16:41:57 -0300

    mvc mostrando perfil

---
+++ lista12/laravel/resources/views/perfil/perfil.blade.php
@@ -0,0 +1,5 @@
+<h4>Usuario</h4>
+@foreach ($usuarios as $key => $usuario)
+<h4>{{$key}}</h4> {{$usuario}}
+@endforeach
+
--- lista12/laravel/routes/web.php
+++ lista12/laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\{UsuarioController};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/perfil', [UsuarioController::class, 'index'])->name('perfil');
+Route::get('/documentos', [DocumentoController::class, 'index'])->name('documentos');
