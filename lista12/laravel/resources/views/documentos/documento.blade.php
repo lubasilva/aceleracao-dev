0a1e269bfdb29689b7544a9a5de1cc9877b8edba
Lucas <lubapisilva@gmail.com>
2021-12-13 22:04:51 -0300

    link para redirecionar rota com id e vizualizacao do documento unico

--- lista12/laravel/resources/views/documentos/documento.blade.php
+++ lista12/laravel/resources/views/documentos/documento.blade.php
@@ -1,14 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+@include('includes.header')
+    <div class="container">
+        <h3 class="text-primary text-center"> Documentos </h3>
+        <table class="table table-bordered mt-5">
+            <thead>
+                <tr>
+                <th scope="col">Titulo</th>
+                <th scope="col">Tamanho do documento em MB</th>
+                <th scope="col">N de assinaturas do documento</th>
+                <th scope="col">Assinatura do responsável</th>
+                <th scope="col">Quantidade de páginas (1 ou 10)</th>
+                </tr>
+            </thead>
+            @foreach ($documentos as $documento)
+                <tbody>
+                    <tr>
+                        <td>{{$documento->titulo}}</td>
+                        <td>{{$documento->tamanho_mb}}</td>
+                        <td>{{$documento->numero_assinaturas}}</td>
+                        <td>{{$documento->assinatura_responsavel}}</td>
+                        <td>{{$documento->quantidade_paginas}}</td>
+                        <td class="text-center"><a href="{{route('showLog', $documento->id)}}"><button type="button" class="btn btn-primary">Vizualizar</button></a></td>
+                    </tr>
+                </tbody>
+            @endforeach
+        </table>
+    </div>
 
-<body>
-    @foreach ($documentos as $documento)
-        <p>Titulo: {{$documento->titulo}} </p><br>
-        <p>Tamanho do documento em MB: {{$documento->tamanho_mb}} </p><br>
-        <p>Nº de assinaturas do documento: {{$documento->numero_assinaturas}} </p><br>
-        <p>Assinatura do responsável: {{$documento->assinatura_responsavel}} </p><br>
-        <p>Quantidade de páginas (1 ou 10): {{$documento->quantidade_paginas}} </p><br>
-    @endforeach
-</body>
-
-</html>
+@include('includes.footer')
