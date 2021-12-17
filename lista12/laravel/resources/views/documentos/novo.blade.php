33d6464d6e7b7fa9401b217f84eba5851e78a053
Lucas <lubapisilva@gmail.com>
2021-12-17 16:51:26 -0300

    funcao old nos value do input

--- lista12/laravel/resources/views/documentos/novo.blade.php
+++ lista12/laravel/resources/views/documentos/novo.blade.php
@@ -12,30 +12,30 @@
         @csrf
         <div class="mb-3">
             <label for="titulo" class="form-label">Titulo do Documento</label>
-            <input type="text" class="form-control" name="titulo">
+            <input type="text" class="form-control" name="titulo" value="{{old('titulo')}}">
         </div>
 
         <div class="row aling-items-center">
             <div class="col mb-3">
                 <label for="tamanho_mb" class="form-label">Tamanho em MB</label>
-                <input type="number" class="form-control" name="tamanho_mb">
+                <input type="number" class="form-control" name="tamanho_mb" value="{{old('tamanho_mb')}}">
             </div>
 
             <div class="col mb-3">
                 <label class="form-label" for="numero_assinaturas">Numero de Assinaturas</label>
-                <input type="number" class="form-control" name="numero_assinaturas">
+                <input type="number" class="form-control" name="numero_assinaturas" value="{{old('numero_assinaturas')}}">
             </div>
         </div>
 
         <div class="row aling-items-center">
             <div class="col mb-3">
                 <label for="assinatura_responsavel" class="form-label">Assinatura Responsavel</label>
-                <input type="text" class="form-control" name="assinatura_responsavel">
+                <input type="text" class="form-control" name="assinatura_responsavel" value="{{old('assinatura_responsavel')}}">
             </div>
 
             <div class="col mb-3">
                 <label for="quantidade_paginas" class="form-label">Quantidade de Pagina</label>
-                <input type="number" class="form-control" name="quantidade_paginas">
+                <input type="number" class="form-control" name="quantidade_paginas" value="{{old('quantidade_paginas')}}">
             </div>
         </div>
 
