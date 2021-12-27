3f9d6fee6f90158ae4824c4f253f43a3468db724
Lucas <lubapisilva@gmail.com>
2021-12-27 04:25:11 -0300

    Ex 110 create seeder

--- lista12/laravel/database/seeders/DocumentoSeeder.php
+++ lista12/laravel/database/seeders/DocumentoSeeder.php
@@ -15,23 +15,27 @@ class DocumentoSeeder extends Seeder
     public function run()
     {
         Documento::create([
-                'titulo' => 'Titulo 1 seeder',
-                'tamanho_mb' => 12,
-                'numero_assinaturas' => 3,
-                'assinatura_responsavel' => 'Assinatura seeder 1',
-                'quantidade_paginas' => 10,
+            'titulo' => 'Titulo 1 seeder',
+            'tamanho_mb' => 12,
+            'numero_assinaturas' => 3,
+            'assinatura_responsavel' => 'Assinatura seeder 1',
+            'quantidade_paginas' => 10,
+        ]);
 
-                'titulo' => 'Titulo 2 seeder',
-                'tamanho_mb' => 13,
-                'numero_assinaturas' => 4,
-                'assinatura_responsavel' => 'Assinatura seeder 2',
-                'quantidade_paginas' => 10,
+        Documento::create([
+            'titulo' => 'Titulo 2 seeder',
+            'tamanho_mb' => 13,
+            'numero_assinaturas' => 4,
+            'assinatura_responsavel' => 'Assinatura seeder 2',
+            'quantidade_paginas' => 10,
+        ]);
 
-                'titulo' => 'Titulo 3 seeder',
-                'tamanho_mb' => 14,
-                'numero_assinaturas' => 5,
-                'assinatura_responsavel' => 'Assinatura seeder 3',
-                'quantidade_paginas' => 1
+        Documento::create([
+            'titulo' => 'Titulo 3 seeder',
+            'tamanho_mb' => 14,
+            'numero_assinaturas' => 5,
+            'assinatura_responsavel' => 'Assinatura seeder 3',
+            'quantidade_paginas' => 1
         ]);
     }
 }
