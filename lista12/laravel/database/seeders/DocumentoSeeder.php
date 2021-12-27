71021f89406254c0d0e892d7082798cd31df73af
Lucas <lubapisilva@gmail.com>
2021-12-27 05:44:12 -0300

    Ex 111 documento factory

--- lista12/laravel/database/seeders/DocumentoSeeder.php
+++ lista12/laravel/database/seeders/DocumentoSeeder.php
@@ -14,28 +14,30 @@ class DocumentoSeeder extends Seeder
      */
     public function run()
     {
-        Documento::create([
-            'titulo' => 'Titulo 1 seeder',
-            'tamanho_mb' => 12,
-            'numero_assinaturas' => 3,
-            'assinatura_responsavel' => 'Assinatura seeder 1',
-            'quantidade_paginas' => 10,
-        ]);
+        // Documento::create([
+        //     'titulo' => 'Titulo 1 seeder',
+        //     'tamanho_mb' => 12,
+        //     'numero_assinaturas' => 3,
+        //     'assinatura_responsavel' => 'Assinatura seeder 1',
+        //     'quantidade_paginas' => 10,
+        // ]);
 
-        Documento::create([
-            'titulo' => 'Titulo 2 seeder',
-            'tamanho_mb' => 13,
-            'numero_assinaturas' => 4,
-            'assinatura_responsavel' => 'Assinatura seeder 2',
-            'quantidade_paginas' => 10,
-        ]);
+        // Documento::create([
+        //     'titulo' => 'Titulo 2 seeder',
+        //     'tamanho_mb' => 13,
+        //     'numero_assinaturas' => 4,
+        //     'assinatura_responsavel' => 'Assinatura seeder 2',
+        //     'quantidade_paginas' => 10,
+        // ]);
 
-        Documento::create([
-            'titulo' => 'Titulo 3 seeder',
-            'tamanho_mb' => 14,
-            'numero_assinaturas' => 5,
-            'assinatura_responsavel' => 'Assinatura seeder 3',
-            'quantidade_paginas' => 1
-        ]);
+        // Documento::create([
+        //     'titulo' => 'Titulo 3 seeder',
+        //     'tamanho_mb' => 14,
+        //     'numero_assinaturas' => 5,
+        //     'assinatura_responsavel' => 'Assinatura seeder 3',
+        //     'quantidade_paginas' => 1
+        // ]);
+
+        Documento::factory(600)->create();
     }
 }
