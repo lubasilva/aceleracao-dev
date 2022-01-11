9bce763a3a354d43f7b32de6081bb9c333f7e4da
Lucas <lubapisilva@gmail.com>
2022-01-11 10:14:34 -0300

    Analise seeder

--- laravel/laravel/database/seeders/DatabaseSeeder.php
+++ laravel/laravel/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             DocumentoSeeder::class,
-            AssinaturaSeeder::class
+            AssinaturaSeeder::class,
+            AnaliseSeeder::class
         ]);
     }
 }
