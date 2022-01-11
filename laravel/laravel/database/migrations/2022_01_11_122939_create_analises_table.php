80bfd8822624e63401d4d5fa550a6684c1109567
Lucas <lubapisilva@gmail.com>
2022-01-11 10:15:26 -0300

    migration analise

---
+++ laravel/laravel/database/migrations/2022_01_11_122939_create_analises_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnalisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection("pgsql")->create('analises', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_doc');
+            $table->integer('id_assinatura');
+            $table->string('status', 45);
+            $table->dateTime('data_analise');
+            $table->integer('visualizadores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection("pgsql")->dropIfExists('analises');
+    }
+}
