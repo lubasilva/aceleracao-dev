54854edd4c8221278ba53c9e61fd5b105fb7845d
Lucas <lubapisilva@gmail.com>
2022-01-11 10:16:02 -0300

    model Analise

---
+++ laravel/laravel/app/Models/Analise.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Analise extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_doc',
+        'id_assinatura',
+        'status',
+        'data_analise',
+        'visualizadores'
+    ];
+
+    protected $connection = 'pgsql';
+}
