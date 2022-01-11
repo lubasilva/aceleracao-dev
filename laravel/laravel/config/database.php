e0c4c310d09cc35553f683c7242ad0a63aef2f58
Lucas <lubapisilva@gmail.com>
2022-01-11 05:41:34 -0300

    Exercicio 122 nomes do env alterado

--- laravel/laravel/config/database.php
+++ laravel/laravel/config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL_2'),
+            'host' => env('DB_HOST_2', '127.0.0.1'),
+            'port' => env('DB_PORT_2', '5432'),
+            'database' => env('DB_DATABASE_2', 'forge'),
+            'username' => env('DB_USERNAME_2', 'forge'),
+            'password' => env('DB_PASSWORD_2', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
